8f7d9ed3b8fe74c3f1fa3bfb0bfc9c6e06e948ba
Yuri Kuichik <kuichikmis4enko@gmail.com>
2024-04-27 00:45:42 +0300

    add cards

--- src/assets/defaultDataState.js
+++ src/assets/defaultDataState.js
@@ -5,32 +5,302 @@ export default {
             name: "Design",
             cards: [
                 {
-                    id: 11,
-                    title: 'Home business advertising ideas',
-                    description: 'Successful businesses know the importance of building and maintaining good working.'
+                    id: 1,
+                    title: 'Old fashioned recipe for preventing allergies and chemical sensitivities',
+                    description: '',
+                    progress: [
+                        {speed: 2}
+                    ],
+                    urlImage: ''
+
                 },
                 {
-                    id: 12,
-                    title: 'There is no competition',
-                    description: 'This article is floated online with an aim to help you find the best dvd printing solution.'
+                    id: 2,
+                    title: 'Home business advertising ideas',
+                    description: 'Successful businesses know the importance of building and maintaining good working.',
+                    progress: [
+                        {speed: 4},
+                        {speed: 1},
+                        {speed: 3},
+                        {speed: 6},
+                    ],
+                    urlImage: ''
+                },
+                {
+                    id: 3,
+                    title: 'Cosmetic surgery abroad making the right choice',
+                    description: '',
+                    progress: [
+                        {speed: 5}
+                    ],
+                    urlImage: '/src/assets/img/card-image-1.png'
                 }
             ]
         },
         {
             id: 2,
+            name: "Prototip",
+            cards: [
+                {
+                    id: 4,
+                    title: 'Home business advertising ideas',
+                    description: 'Successful businesses know the importance of building and maintaining good working.',
+                    progress: [
+                        {speed: 4},
+                        {speed: 1},
+                        {speed: 3},
+                    ],
+                    urlImage: ''
+                },
+                {
+                    id: 5,
+                    title: 'Unmatched toner cartridge quality 20 less than oem price',
+                    description: 'Why read motivational sayings? For motivation! You might need a bit, if you can use last year’s list of goals this year because it’s as good as new.',
+                    progress: [
+                        {speed: 5},
+                        {speed: 2},
+                    ],
+                    urlImage: '/src/assets/img/card-image-2.png'
+                },
+                {
+                    id: 6,
+                    title: 'How to look up',
+                    description: 'Are you considering buying a compatible inkjet cartridge for your printer?',
+                    progress: [
+                        {speed: 6}
+                    ],
+                    urlImage: '/src/assets/img/card-image-3.png'
+                }
+            ]
+        },
+        {
+            id: 3,
+            name: "Trello",
+            cards: [
+                {
+                    id: 7,
+                    title: 'Types of paper in catalog printing',
+                    description: 'Branding is no longer simply about visual appeal (or the cherry in the apple pie example, as given in my earlier article).',
+                    progress: [
+                        {speed: 3},
+                        {speed: 1},
+                        {speed: 5}
+                    ],
+                    urlImage: '/src/assets/img/card-image-4.png'
+                },
+                {
+                    id: 8,
+                    title: 'There is no competition',
+                    description: 'This article is floated online with an aim to help you find the best dvd printing solution.',
+                    progress: [
+                        {speed: 4},
+                        {speed: 6}
+                    ],
+                    urlImage: ''
+                }
+            ]
+        },
+        {
+            id: 4,
             name: "Test",
             cards: [
                 {
+                    id: 9,
+                    title: 'Linux or windows which is it',
+                    description: 'Saving money – is something we would all like.',
+                    progress: [
+                        {speed: 5},
+                        {speed: 1}
+                    ],
+                    urlImage: ''
+                },
+                {
+                    id: 10,
+                    title: 'Be single minded',
+                    description: 'Create a list with all possible keywords that fit to your product, service or business field.',
+                    progress: [
+                        {speed: 3},
+                        {speed: 1},
+                        {speed: 5},
+                    ],
+                    urlImage: '/src/assets/img/card-image-5.png'
+                },
+                {
                     id: 11,
-                    title: 'Home business advertising ideas',
-                    description: 'Successful businesses know the importance of building and maintaining good working.'
+                    title: 'Linux or windows which is it',
+                    description: 'Saving money – is something we would all like.',
+                    progress: [
+                        {speed: 2}
+                    ],
+                    urlImage: ''
                 },
                 {
                     id: 12,
-                    title: 'There is no competition',
-                    description: 'This article is floated online with an aim to help you find the best dvd printing solution.'
+                    title: 'Dna the future of nutrition',
+                    description: '“Why does anyone want a vasectomy reversal?” This is a question I hear any time I tell someone what I do for a living.',
+                    progress: [
+                        {speed: 4},
+                        {speed: 6}
+                    ],
+                    urlImage: ''
                 }
             ]
-        }
+        },
+        {
+            id: 5,
+            name: "Final",
+            cards: [
+                {
+                    id: 13,
+                    title: 'At home treatments for beauty on a budget',
+                    description: 'The holidays bring with them thoughts of carving and serving delicious turkey dinners to your family and friends. Tradition has its fans, but perhaps this year you’d like to try a twist to your turkey recipe.',
+                    progress: [
+                        {speed: 2},
+                        {speed: 1},
+                        {speed: 3},
+                    ],
+                    urlImage: ''
+                },
+                {
+                    id: 14,
+                    title: 'Home business advertising ideas',
+                    description: 'Successful businesses know the importance of building and maintaining good working.',
+                    progress: [
+                        {speed: 3},
+                        {speed: 6}
+                    ],
+                    urlImage: ''
+                },
+                {
+                    id: 15,
+                    title: 'Tips for choosing the perfect gloss for your lips',
+                    description: 'With MySpace becoming more popular every day, there is the constant need to be different. There are millions of users.',
+                    progress: [
+                        {speed: 5},
+                        {speed: 2}
+                    ],
+                    urlImage: '/src/assets/img/card-image-6.png'
+                }
+            ]
+        },
+        {
+            id: 6,
+            name: "Design",
+            cards: [
+                {
+                    id: 16,
+                    title: 'Old fashioned recipe for preventing allergies and chemical sensitivities',
+                    description: '',
+                    progress: [
+                        {speed: 2}
+                    ],
+                    urlImage: ''
+
+                },
+                {
+                    id: 17,
+                    title: 'Home business advertising ideas',
+                    description: 'Successful businesses know the importance of building and maintaining good working.',
+                    progress: [
+                        {speed: 4},
+                        {speed: 1},
+                        {speed: 3},
+                        {speed: 6},
+                    ],
+                    urlImage: ''
+                },
+                {
+                    id: 18,
+                    title: 'Cosmetic surgery abroad making the right choice',
+                    description: '',
+                    progress: [
+                        {speed: 5}
+                    ],
+                    urlImage: '/src/assets/img/card-image-1.png'
+                }
+            ]
+        },
+        {
+            id: 7,
+            name: "Prototip",
+            cards: [
+                {
+                    id: 19,
+                    title: 'Home business advertising ideas',
+                    description: 'Successful businesses know the importance of building and maintaining good working.',
+                    progress: [
+                        {speed: 4},
+                        {speed: 1},
+                        {speed: 3},
+                    ],
+                    urlImage: ''
+                },
+                {
+                    id: 20,
+                    title: 'Unmatched toner cartridge quality 20 less than oem price',
+                    description: 'Why read motivational sayings? For motivation! You might need a bit, if you can use last year’s list of goals this year because it’s as good as new.',
+                    progress: [
+                        {speed: 5},
+                        {speed: 2},
+                    ],
+                    urlImage: '/src/assets/img/card-image-2.png'
+                },
+                {
+                    id: 21,
+                    title: 'How to look up',
+                    description: 'Are you considering buying a compatible inkjet cartridge for your printer?',
+                    progress: [
+                        {speed: 6}
+                    ],
+                    urlImage: '/src/assets/img/card-image-3.png'
+                }
+            ]
+        },
+        {
+            id: 8,
+            name: "Test",
+            cards: [
+                {
+                    id: 22,
+                    title: 'Linux or windows which is it',
+                    description: 'Saving money – is something we would all like.',
+                    progress: [
+                        {speed: 5},
+                        {speed: 1}
+                    ],
+                    urlImage: ''
+                },
+                {
+                    id: 23,
+                    title: 'Be single minded',
+                    description: 'Create a list with all possible keywords that fit to your product, service or business field.',
+                    progress: [
+                        {speed: 3},
+                        {speed: 1},
+                        {speed: 5},
+                    ],
+                    urlImage: '/src/assets/img/card-image-5.png'
+                },
+                {
+                    id: 24,
+                    title: 'Linux or windows which is it',
+                    description: 'Saving money – is something we would all like.',
+                    progress: [
+                        {speed: 2}
+                    ],
+                    urlImage: ''
+                },
+                {
+                    id: 25,
+                    title: 'Dna the future of nutrition',
+                    description: '“Why does anyone want a vasectomy reversal?” This is a question I hear any time I tell someone what I do for a living.',
+                    progress: [
+                        {speed: 4},
+                        {speed: 6}
+                    ],
+                    urlImage: ''
+                }
+            ]
+        },
     ]
 }
---
+++ src/components/CardItem.vue
@@ -0,0 +1,174 @@
+<script setup>
+import { defineProps } from 'vue';
+
+import CardProgressBar from './CardProgressBar.vue';
+
+defineProps({
+    model: {
+        type: Object,
+        default: () => ({speed: 6})
+    },
+    newCard: {
+        type: Boolean,
+        drfault: false
+    }
+})
+
+const getRandomInt = () => {
+    return Math.floor(Math.random() * 100);
+}
+
+const addMassage = () => {
+    // do something
+}
+
+const noteLikeCard = () => {
+    // do something
+}
+
+const attachFile = () => {
+    // do something
+}
+
+const addCard = () => {
+    alert('addCard');
+}
+
+const editCard = () => {
+    alert('editCard');
+}
+
+</script>
+
+<template>
+    <div v-if="newCard" @click="addCard" class="card-item card-item_empty">
+        <div>+ Add new card</div>
+    </div>
+
+    <div v-else class="card-item" @click="editCard">
+        <div v-if="model.urlImage" class="card-item__image">
+            <img alt="card image" :src="model.urlImage" />
+        </div>
+
+        <div>
+            <CardProgressBar :progress="model.progress"/>
+            <div class="card-item__title">
+                {{ model.title }}
+            </div>
+            <div v-if="model.description" class="card-item__description sh-p2">
+                {{ model.description }}
+            </div>
+        </div>
+
+       
+        <div class="card-item__footer">
+            <div class="card-item__users">users</div>
+
+            <div class="card-item__icons">
+                <div class="l-box_inline l-box_aic" @click.stop="addMassage">
+                    <span class="sh-p3">{{ getRandomInt() }}</span>
+                    <img alt="message icon" src="/src/assets/icons/message-square-outline.svg" />
+                </div>
+                <div class="l-box_inline l-box_aic" @click.stop="noteLikeCard">
+                    <span class="sh-p3">{{ getRandomInt() }}</span>
+                    <img alt="heart icon" src="/src/assets/icons/heart-outline.svg" />
+                </div>
+                <div class="l-box_inline l-box_aic" @click.stop="attachFile">
+                    <span class="sh-p3">{{ getRandomInt() }}</span>
+                    <img alt="attach icon" src="/src/assets/icons/attach-outline.svg" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.card-item {
+    $this: &;
+
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    padding: 16px;
+    background: var(--color-background-grey);
+    border-radius: 10px;
+    cursor: pointer;
+
+    &#{$this}_empty {
+        justify-content: center;
+        align-items: center;
+        height: 160px;
+        background: transparent;
+        border: 1px dashed var(--color-border-emphasis);
+    }
+
+    &__image  {
+        height: 170px;
+        border-radius: 9px;
+        overflow: hidden;
+
+        img {
+            height: 100%;
+            width: 100%;
+            max-width: 100%;
+            object-fit: cover;
+        } 
+    }
+
+    &__title {
+        margin-top: 6px;
+    }
+
+    &__description {
+        margin-top: 2px;
+        color: var(--color-text-medium);
+    }
+
+    &__footer {
+        display: flex;
+        justify-content: space-between;
+        padding-top: 8px;
+    }
+
+    &__icons {
+        display: flex;
+        justify-content: space-between;
+        width: 122px;
+        color: var(--color-text-light);
+
+        img {
+            height: 16px;
+            width: 16px;
+            margin-left: 1px;
+        }
+    }
+
+    @media (min-width: 770px) {
+        gap: 16px;
+        padding: 20px;
+
+        &__title {
+            margin-top: 9px;
+        }
+
+        &__description {
+            margin-top: 3px;
+        }
+
+        &__footer {
+            padding-top: 11px;
+        }
+
+        &__icons {
+            width: 152px;
+
+            img {
+            height: 20px;
+            width: 20px;
+            margin-left: 1px;
+        }
+        }
+    }
+}
+
+</style>
